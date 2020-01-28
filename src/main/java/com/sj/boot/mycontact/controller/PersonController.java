a5f600efd97af842e2a0facb552cbddddb1cf99d
sangjin0309 <sangjin0309>
2020-01-29 00:10:11 +0900

    컨트롤러 활용1

---
+++ src/main/java/com/sj/boot/mycontact/controller/PersonController.java
@@ -0,0 +1,20 @@
+package com.sj.boot.mycontact.controller;
+
+import com.sj.boot.mycontact.domain.Person;
+import com.sj.boot.mycontact.service.PersonService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RequestMapping(value = "/api/person")
+@RestController
+public class PersonController {
+
+    @Autowired
+    PersonService personService;
+
+    @GetMapping("/{id}")
+    public Person getPerson(@PathVariable Long id) {
+        return personService.getPerson(id);
+    }
+
+}
--- src/main/java/com/sj/boot/mycontact/service/PersonService.java
+++ src/main/java/com/sj/boot/mycontact/service/PersonService.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -29,7 +30,8 @@ public class PersonService {
 
     @Transactional(readOnly = true)
     public Person getPerson(Long id) {
-        Person person = personRepository.findById(id).get();
+        // get을 하는데 값이 없으면 null을 리턴
+        Person person = personRepository.findById(id).orElse(null);
 
         log.info("person : {}", person);
 
